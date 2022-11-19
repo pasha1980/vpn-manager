35e09472784842c66eb0d448b75fe12d053535d4
pashka <hvalyginpavel@gmail.com>
2022-11-19 16:22:44 +0200

    Fix volumes in test script

--- run-test.sh
+++ run-test.sh
@@ -4,10 +4,12 @@ docker rm test || true
 
 docker build -f devops/docker/Dockerfile -t test .
 docker run --cap-add=NET_ADMIN \
--v data:/opt \
+-v ./tmp/openvpn_data:/opt/openvpn_data \
+-v ./tmp/openvpn_config:/etc/openvpn \
 -p 1194:1194/udp \
--p 8080:80 \
+-p 8080:80/tcp \
 -e HOST_ADDR=127.0.0.1 \
+-e HOST_URL=127.0.0.1:8080 \
 --rm \
 --name test \
 test "$@"
