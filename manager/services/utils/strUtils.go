12569466769f136255edfb29a53ef76a81f362f4
pashka <hvalyginpavel@gmail.com>
2022-11-21 17:22:47 +0200

    Improve secret generation

--- manager/services/utils/strUtils.go
+++ manager/services/utils/strUtils.go
@@ -2,11 +2,14 @@ package utils
 
 import (
 	"math/rand"
+	"time"
 )
 
 var charset = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func GenerateRandomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+
 	b := make([]rune, length)
 	for i := range b {
 		b[i] = charset[rand.Intn(len(charset))]
