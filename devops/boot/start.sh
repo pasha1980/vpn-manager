79d1729c8425c865a89cd29ba0e74ede035149ce
pashka <hvalyginpavel@gmail.com>
2022-11-23 13:01:55 +0200

    Make HTTP_ADDR and HOST_URL optional parameters

--- devops/boot/start.sh
+++ devops/boot/start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+echo "Initialising environment..."
+
+if [[ -z $HOST_ADDR ]]; then
+  export HOST_ADDR=$(curl ifconfig.me) >> /dev/null
+fi
+
+if [[ -z $HOST_URL ]]; then
+  export HOST_URL="http://$HOST_ADDR:8080"
+fi
+
+echo
+
 echo "Initialising VPN services..."
 echo
 
--- run-test.sh
+++ run-test.sh
@@ -3,13 +3,14 @@ docker stop test || true
 docker rm test || true
 
 docker build -f devops/docker/Dockerfile -t test .
+
+docker volume create openvpn_data
+docker volume create openvpn_config
 docker run --cap-add=NET_ADMIN \
--v ./tmp/openvpn_data:/opt/openvpn_data \
--v ./tmp/openvpn_config:/etc/openvpn \
+-v openvpn_data:/opt/openvpn_data \
+-v openvpn_config:/etc/openvpn \
 -p 1194:1194/udp \
 -p 8080:80/tcp \
--e HOST_ADDR=127.0.0.1 \
--e HOST_URL=127.0.0.1:8080 \
 --rm \
 --name test \
 test "$@"
